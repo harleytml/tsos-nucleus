b63878c6fd18be351c4b608864658ff3e10d4a75
tsukisuperior@bitbucket <tsukisuperior@gmail.com>
2021-01-10 14:04:59 -0600

    added some buildscript changes so it may compile system apps

--- build.sh
+++ build.sh
@@ -101,6 +101,14 @@ mkdir -pv $FILESYSTEM_ROOT/{bin,cfg,lib,sys,sys/tmp,sys/headers,sys/info,sys/tra
 mkdir -pv $CODE_DIR/build/
 cd ./nucleus/
 make -f Makefile.$1.mk
+cd $CODE_DIR
+cd applications
 
 #Now we need to make the system applications
-SYSTEM_APPLICATIONS={cd,cpy,ctl,date,del,echo,info,kill,lstd,lstp,lstv,math,mov,osi,pi,tsh}
+SYSTEM_APPLICATIONS=(cd cpy ctl date del echo info kill lstd lstp lstv math mov osi pi tsh)
+for i in "${SYSTEM_APPLICATIONS[@]}"
+do
+   cd $i
+   make -f Makefile.$1.mk
+   cd ..
+done
