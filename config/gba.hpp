5303e58d9a1e730dacc41ee26a2194c4f00db5e0
tsuki-superior <tsukisuperior@tutanota.com>
2021-02-19 22:52:32 +0000

    fixed some invalid config files

--- config/gba.hpp
+++ config/gba.hpp
@@ -1,34 +1,34 @@
 #define MACHINE GAMEBOY_ADVANCED
 #define VIDEO_DRIVERS       \
     {                       \
-        GBA_SCREEN_driver() \
+        new GBA_SCREEN_driver() \
     }
 #define INPUT_DRIVERS        \
     {                        \
-        GBA_GAMEPAD_driver() \
+        new GBA_GAMEPAD_driver() \
     }
 #define DISK_DRIVERS           \
     {                          \
-        GBA_CARTRIDGE_driver() \
+        new GBA_CARTRIDGE_driver() \
     }
 #define SERIAL_DRIVERS       \
     {                        \
-        GBA_IO_PORT_driver() \
+        new GBA_IO_PORT_driver() \
     }
 #define BOOT_DRIVERS      \
     {                     \
-        GBA_BOOT_driver() \
+        new GBA_BOOT_driver() \
     }
 #define FILESYSTEM_DRIVERS                                            \
     {                                                                 \
-        FAT12_driver(), FAT16_driver(), FAT32_driver(), CDFS_driver() \
+        new FAT12_driver(), new FAT16_driver(), new FAT32_driver(), new CDFS_driver() \
     }
 #define SOUND_DRIVERS \
     {                 \
     }
 #define PROCESS_DRIVERS \
     {                   \
-        ELF_driver()    \
+        new ELF_driver()    \
     }
 #define VIDEO_DRIVER_COUNT 1
 #define INPUT_DRIVER_COUNT 1
--- config/pc.hpp
+++ config/pc.hpp
@@ -1,51 +1,34 @@
 #define MACHINE PERSONAL_COMPUTER
-
-extern void MDA_driver();
-extern void CGA_driver();
-extern void EGA_driver();
-extern void VGA_driver();
-extern void XT_KEYBOARD_driver();
-extern void AT_KEYBOARD_driver();
-extern void CD_driver();
-extern void RS232_driver();
-extern void GRUB2_driver();
-extern void FAT12_driver();
-extern void FAT16_driver();
-extern void FAT32_driver();
-extern void UDF_driver();
-extern void ELF_driver();
-extern void SFS_driver();
-
 #define VIDEO_DRIVERS                                          \
     {                                                          \
-        MDA_driver(), CGA_driver(), EGA_driver(), VGA_driver() \
+        new MDA_driver(), new CGA_driver(), new EGA_driver(), new VGA_driver() \
     }
 #define INPUT_DRIVERS                              \
     {                                              \
-        XT_KEYBOARD_driver(), AT_KEYBOARD_driver() \
+        new XT_KEYBOARD_driver(), new AT_KEYBOARD_driver() \
     }
 #define DISK_DRIVERS \
     {                \
-        CD_driver()  \
+        new CD_driver()  \
     }
 #define SERIAL_DRIVERS \
     {                  \
-        RS232_driver() \
+        new RS232_driver() \
     }
 #define BOOT_DRIVERS   \
     {                  \
-        GRUB2_driver() \
+        new GRUB2_driver() \
     }
 #define FILESYSTEM_DRIVERS                                                         \
     {                                                                              \
-        FAT12_driver(), FAT16_driver(), FAT32_driver(), UDF_driver(), SFS_driver() \
+        new FAT12_driver(), new FAT16_driver(), new FAT32_driver(), new UDF_driver(), new SFS_driver() \
     }
 #define SOUND_DRIVERS \
     {                 \
     }
 #define PROCESS_DRIVERS \
     {                   \
-        ELF_driver()    \
+        new ELF_driver()    \
     }
 #define VIDEO_DRIVER_COUNT 4
 #define INPUT_DRIVER_COUNT 2
