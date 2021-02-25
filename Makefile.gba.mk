03aa4876275f1b65a648b4bc81f7abf293c547ab
tsuki-superior <tsukisuperior@tutanota.com>
2021-02-24 18:54:48 -0600

    changes to almost get gba as far as pc

--- Makefile.gba.mk
+++ Makefile.gba.mk
@@ -2,26 +2,18 @@
 # By Tsuki Superior
 .RECIPEPREFIX=:
 
-BUILD_DIR=$(PWD)/../build
-NUCLEUS_DIR=$(PWD)/../src
-CONFIG_DIR=$(PWD)/../config
-LINKER_SCRIPTS_DIR=$(PWD)/../linker-scripts
-
-BOOT_CORE=$(BUILD_DIR)/boot_driver.o $(BUILD_DIR)/boot_module.o
-DISK_CORE=$(BUILD_DIR)/disk_driver.o $(BUILD_DIR)/disk_module.o
-FILESYSTEM_CORE=$(BUILD_DIR)/filesystem_driver.o $(BUILD_DIR)/filesystem_module.o $(BUILD_DIR)/filesystem_file.o
-INPUT_CORE=$(BUILD_DIR)/input_driver.o $(BUILD_DIR)/input_module.o
-PROCESS_CORE=$(BUILD_DIR)/process_driver.o $(BUILD_DIR)/process_module.o
-SERIAL_CORE=$(BUILD_DIR)/serial_driver.o $(BUILD_DIR)/serial_module.o
-SOUND_CORE=$(BUILD_DIR)/sound_driver.o $(BUILD_DIR)/sound_module.o $(BUILD_DIR)/sound_tone.o 
-VIDEO_CORE=$(BUILD_DIR)/video_driver.o $(BUILD_DIR)/video_module.o $(BUILD_DIR)/video_color.o $(BUILD_DIR)/video_font.o
-DRIVER_LIST=$(BUILD_DIR)/sfs.o $(BUILD_DIR)/gba_boot.o $(BUILD_DIR)/udf.o $(BUILD_DIR)/fat12.o $(BUILD_DIR)/fat16.o $(BUILD_DIR)/fat32.o $(BUILD_DIR)/gba_gamepad.o $(BUILD_DIR)/elf.o $(BUILD_DIR)/gba_io_port.o $(BUILD_DIR)/gba_screen.o
+BUILD_DIR:=../build
+SRC_DIR:=../src
+ASM_DIR:=../asm
+INCLUDE_DIR:=../include
+CONFIG_DIR:=../config
+LINKER_SCRIPTS_DIR:=../linker-scripts
 
 CC:=tsos-armeabi-gcc
-CC_FLAGS:=-g -std=c99 -ffreestanding -O2 -Wall -Wextra -pedantic -mcpu=arm7tdmi -nostartfiles -mthumb-interwork
+CC_FLAGS:=-g -I $(INCLUDE_DIR) -I $(CONFIG_DIR) -I ./ -std=c99 -ffreestanding -O2 -Wall -Wextra -pedantic -mcpu=arm7tdmi -nostartfiles -mthumb-interwork -c
 
 CPP:=tsos-armeabi-g++
-CPP_FLAGS:=-g -std=c++20 -ffreestanding -O2 -Wall -Wextra -Wno-write-strings -Wno-unused-parameter -fno-exceptions -fno-rtti -nostdlib -lgcc -pedantic -mcpu=arm7tdmi -nostartfiles -mthumb-interwork
+CPP_FLAGS:=-g -I $(INCLUDE_DIR) -I $(CONFIG_DIR) -I ./ -std=c++20 -ffreestanding -O2 -Wall -Wextra -Wno-write-strings -Wno-unused-parameter -fno-exceptions -fno-rtti -nostdlib -lgcc -pedantic -mcpu=arm7tdmi -nostartfiles -mthumb-interwork -c
 
 AS:=tsos-armeabi-as
 AS_FLAGS:=-mcpu=arm7tdmi -mthumb-interwork
@@ -29,17 +21,19 @@ AS_FLAGS:=-mcpu=arm7tdmi -mthumb-interwork
 LD:=tsos-armeabi-ld.gold
 LD_FLAGS:=-g -T $(LINKER_SCRIPTS_DIR)/gba-elf.ld -mcpu=arm7tdmi -nostartfiles -mthumb-interwork -ffreestanding -O2 -nostdlib -lgcc
 
-CPP_FILES:=$(shell find $(NUCLEUS_DIR) -name \*.cpp)
-OBJ_FILES:=${CPP_FILES:.cpp=.o}
+CPP_FILES:=$(wildcard $(SRC_DIR)/*.cpp)
+OBJ_FILES:=$(patsubst %.cpp, $(BUILD_DIR)/%.o, $(CPP_FILES))
+
+default: $(BUILD_DIR)/nucleus.tse
 
 $(BUILD_DIR)/nucleus.tse: $(OBJ_FILES) $(BUILD_DIR)/bootloader.o 
 :$(CPP) $(LD_FLAGS) -o $@ $^ 
 
-$(BUILD_DIR)/%.o: $(CPP_FILES)
+$(BUILD_DIR)/%.o: $(SRC_DIR)/%.cpp
 :$(CPP) $(CPP_FLAGS) -o $@ $^ 
 
-$(BUILD_DIR)/bootloader.o: $(NUCLEUS_DIR)/drivers/boot/gba_boot/gba_boot.asm 
-:$(AS) $(AS_FLAGS) -o $@ $^
+$(BUILD_DIR)/bootloader.o: $(ASM_DIR)/gba_boot.asm 
+:$(AS) $(AS_FLAGS) -o $@ -c $^
 
 clean:
-:rm -rfv $(shell find $(NUCLEUS_DIR) -name \*.o)
+:rm -rfv $(shell find ../ -name \*.o)
--- Makefile.pc.mk
+++ Makefile.pc.mk
@@ -10,10 +10,10 @@ CONFIG_DIR:=../config
 LINKER_SCRIPTS_DIR:=../linker-scripts
 
 CC:=tsos-i686-gcc
-CC_FLAGS:=-g -I $(INCLUDE_DIR) -I $(CONFIG_DIR) -I ./ -std=c99 -ffreestanding -O3 -Wall -Wextra -pedantic -mtune=i686 -mfpmath=387 -m32
+CC_FLAGS:=-g -I $(INCLUDE_DIR) -I $(CONFIG_DIR) -I ./ -std=c99 -ffreestanding -O3 -Wall -Wextra -pedantic -mtune=i686 -mfpmath=387 -m32 -c
 
 CPP:=tsos-i686-g++
-CPP_FLAGS:=-g -I $(INCLUDE_DIR) -I $(CONFIG_DIR) -I ./ -std=c++20 -ffreestanding -O3 -Wall -Wextra -Wno-write-strings -Wno-return-type -Wno-int-to-pointer-cast -Wno-unused-parameter -fno-exceptions -fno-rtti -nostdlib -lgcc -pedantic -mtune=i686 -mfpmath=387 -m32
+CPP_FLAGS:=-g -I $(INCLUDE_DIR) -I $(CONFIG_DIR) -I ./ -std=c++20 -ffreestanding -O3 -Wall -Wextra -Wno-write-strings -Wno-return-type -Wno-int-to-pointer-cast -Wno-unused-parameter -fno-exceptions -fno-rtti -nostdlib -lgcc -pedantic -mtune=i686 -mfpmath=387 -m32 -c
 
 AS:=tsos-i686-as
 AS_FLAGS:=-g -mtune=i686
@@ -27,13 +27,13 @@ OBJ_FILES:=$(patsubst %.cpp, $(BUILD_DIR)/%.o, $(CPP_FILES))
 default: $(BUILD_DIR)/nucleus.tse
 
 $(BUILD_DIR)/nucleus.tse: $(OBJ_FILES) $(BUILD_DIR)/bootloader.o 
-:$(CPP) $(LD_FLAGS) -o $@ -c $^ 
+:$(CPP) $(LD_FLAGS) -o $@ $^ 
 
 $(BUILD_DIR)/%.o: $(SRC_DIR)/%.cpp
-:$(CPP) $(CPP_FLAGS) -o $@ -c $^ 
+:$(CPP) $(CPP_FLAGS) -o $@ $^ 
 
 $(BUILD_DIR)/bootloader.o: $(ASM_DIR)/grub2.asm 
 :$(AS) $(AS_FLAGS) -o $@ -c $^
 
 clean:
-:rm -rfv $(shell find $(SRC_DIR) -name \*.o)
+:rm -rfv $(shell find ../ -name \*.o)
