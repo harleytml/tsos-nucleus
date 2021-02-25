bf7b3d44a12d2bef66398bf38fa7ecfbcd406f78
tsuki-superior <tsukisuperior@tutanota.com>
2021-02-24 18:20:02 -0600

    fix

--- Makefile.pc.mk
+++ Makefile.pc.mk
@@ -2,12 +2,12 @@
 # By Tsuki Superior
 .RECIPEPREFIX=:
 
-BUILD_DIR:=$(PWD)/../build
-SRC_DIR:=$(PWD)/../src
-ASM_DIR:=$(PWD)/../asm
-INCLUDE_DIR:=$(PWD)/../include
-CONFIG_DIR:=$(PWD)/../config
-LINKER_SCRIPTS_DIR:=$(PWD)/../linker-scripts
+BUILD_DIR:=../build
+SRC_DIR:=../src
+ASM_DIR:=../asm
+INCLUDE_DIR:=../include
+CONFIG_DIR:=../config
+LINKER_SCRIPTS_DIR:=../linker-scripts
 
 CC:=tsos-i686-gcc
 CC_FLAGS:=-g -I $(INCLUDE_DIR) -I $(CONFIG_DIR) -I ./ -std=c99 -ffreestanding -O3 -Wall -Wextra -pedantic -mtune=i686 -mfpmath=387 -m32
@@ -22,12 +22,14 @@ LD:=tsos-i686-ld.gold
 LD_FLAGS:=-g -T $(LINKER_SCRIPTS_DIR)/pc-elf.ld -nostartfiles -mtune=i686 -mfpmath=387 -m32 -ffreestanding -O2 -nostdlib -lgcc
 
 CPP_FILES:=$(wildcard $(SRC_DIR)/*.cpp)
-OBJ_FILES:=$(CPP_FILES:$(SRC_DIR)/%.cpp=$(BUILD_DIR)/%.o)
+OBJ_FILES:=$(patsubst %.cpp, $(BUILD_DIR)/%.o, $(CPP_FILES))
+
+default: $(BUILD_DIR)/nucleus.tse
 
 $(BUILD_DIR)/nucleus.tse: $(OBJ_FILES) $(BUILD_DIR)/bootloader.o 
 :$(CPP) $(LD_FLAGS) -o $@ $^ 
 
-$(BUILD_DIR)/%.o: $(CPP_FILES)
+$(BUILD_DIR)/%.o: $(SRC_DIR)/%.cpp
 :$(CPP) $(CPP_FLAGS) -o $@ $^ 
 
 $(BUILD_DIR)/bootloader.o: $(ASM_DIR)/grub2.asm 
