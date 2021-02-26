907c2e7493b2847a15ce2ceb2c6a83b96045a237
tsuki-superior <tsukisuperior@tutanota.com>
2021-02-26 15:57:53 -0600

    added strcmp

--- src/generic/icxxabi.cpp
+++ src/generic/icxxabi.cpp
@@ -210,4 +210,20 @@ extern "C"
 		}
 		return dstptr;
 	}
+	int strcmp(const char *p1, const char *p2)
+	{
+		const uint8_t *s1 = (const uint8_t *)p1;
+		const uint8_t *s2 = (const uint8_t *)p2;
+		uint8_t c1, c2;
+		do
+		{
+			c1 = (uint8_t)*s1++;
+			c2 = (uint8_t)*s2++;
+			if (c1 == NULL)
+			{
+				return c1 - c2;
+			}
+		} while (c1 == c2);
+		return c1 - c2;
+	}
 }
