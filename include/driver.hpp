ad816887a936b79e659dc7e017136925f612f63d
tsuki-superior <tsukisuperior@tutanota.com>
2021-03-04 07:45:56 -0600

    formatting

--- include/driver.hpp
+++ include/driver.hpp
@@ -1,31 +1,30 @@
 #ifndef __TSOS_DRIVER_CORE__
 #define __TSOS_DRIVER_CORE__
 
-      #include "types.hpp"
+#include "types.hpp"
 
-      class Driver
-        {
-        public:
-            Driver();
-            ~Driver();
+class Driver
+{
+public:
+    Driver();
+    ~Driver();
 
-            virtual void Activate();
-            virtual int Reset();
-            virtual void Deactivate();
-        };
+    virtual void Activate();
+    virtual int Reset();
+    virtual void Deactivate();
+};
 
-        class DriverManager
-        {
-        public:
-            Driver* drivers[265];
-            int numDrivers;
-            
-        public:
-            DriverManager();
-            void AddDriver(Driver*);
-            
-            void ActivateAll();
-            
-        };
+class DriverManager
+{
+public:
+    Driver *drivers[265];
+    int numDrivers;
+
+public:
+    DriverManager();
+    void AddDriver(Driver *);
+
+    void ActivateAll();
+};
 
 #endif
