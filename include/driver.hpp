50ad22e3e63fa32cc7e032399bdd85dd08111158
AptRock327 <61791835+AptRock327@users.noreply.github.com>
2021-03-04 08:55:36 +0100

    driver.hpp moment

--- include/driver.hpp
+++ include/driver.hpp
@@ -1,24 +1,31 @@
-//By Tsuki Superior
 #ifndef __TSOS_DRIVER_CORE__
 #define __TSOS_DRIVER_CORE__
 
-#include "types.hpp"
+      #include "types.hpp"
 
-// The class for drivers
-class Driver
-{
-public:
-  // Constructor
-  Driver(void);
+      class Driver
+        {
+        public:
+            Driver();
+            ~Driver();
 
-  // Destructor
-  ~Driver();
+            virtual void Activate();
+            virtual int Reset();
+            virtual void Deactivate();
+        };
 
-  // A function called to determine if a driver should be used.
-  virtual bool detectsystem(void) = 0;
-
-  // The name of said driver
-  char *name;
-};
+        class DriverManager
+        {
+        public:
+            Driver* drivers[265];
+            int numDrivers;
+            
+        public:
+            DriverManager();
+            void AddDriver(Driver*);
+            
+            void ActivateAll();
+            
+        };
 
 #endif
