bf7704e16ea2f16a32c31ed188945791f9e4d442
AptRock327 <61791835+AptRock327@users.noreply.github.com>
2021-03-04 09:16:39 +0100

    A header for handlers

---
+++ include/handlers.hpp
@@ -0,0 +1,37 @@
+#include "types.hpp"
+
+class MouseHandlerBasic : public MouseEventHandler
+{
+    int8_t x, y;
+public:
+    
+    MouseToConsole()
+    {
+        uint16_t* VideoMemory = (uint16_t*)0xb8000;
+        x = 40;
+        y = 12;
+        VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0x0F00) << 4
+                            | (VideoMemory[80*y+x] & 0xF000) >> 4
+                            | (VideoMemory[80*y+x] & 0x00FF);        
+    }
+    
+    virtual void OnMouseMove(int xoffset, int yoffset)
+    {
+        static uint16_t* VideoMemory = (uint16_t*)0xb8000;
+        VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0x0F00) << 4
+                            | (VideoMemory[80*y+x] & 0xF000) >> 4
+                            | (VideoMemory[80*y+x] & 0x00FF);
+
+        x += xoffset;
+        if(x >= 80) x = 79;
+        if(x < 0) x = 0;
+        y += yoffset;
+        if(y >= 25) y = 24;
+        if(y < 0) y = 0;
+
+        VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0x0F00) << 4
+                            | (VideoMemory[80*y+x] & 0xF000) >> 4
+                            | (VideoMemory[80*y+x] & 0x00FF);
+    }
+    
+};
