a5c04588d3d649656455ab66c07f56a9ed995f29
tsuki-superior <tsukisuperior@tutanota.com>
2021-03-26 22:35:43 -0500

    the build config gets more complex

--- build.py
+++ build.py
@@ -13,9 +13,19 @@ if os.path.isdir("/tmp/tsos-filesystem"):
     shutil.rmtree("/tmp/tsos-filesystem")
 
 # Open that config file and read from it
-build_settings_file = open("cfg/build.json")
-build_settings = json.loads(build_settings_file.read())
-build_settings_file.close()
+tmp_file = open("cfg/system_config.json")
+build_settings = json.loads(tmp_file.read())
+tmp_file.close()
+
+# Open up the directory structure file
+tmp_file = open("cfg/directory_structure.json")
+directory_structure = json.loads(tmp_file.read())
+tmp_file.close()
+
+# Read the deploy config file
+tmp_file = open("cfg/deploy_config.json")
+deploy_configs = json.loads(tmp_file.read())
+tmp_file.close()
 
 # Make sure the most important part is there
 if not "systems" in build_settings:
@@ -73,80 +83,76 @@ if platform in build_settings["systems"]:
         cprint("Compiling failed!", "red")
         exit(1)
 
-# Building was succesful if it made it to here
-cprint("Compiling and deploying completed.", "green")
-cprint("Your copy of the TS/OS nucleus is at build/nucleus", "green")
+# Make sure the correct option is in the config
+if "deploy_type" in build_settings["systems"][platform]:
 
-if "deploy_disk_type" in build_settings["systems"][platform]["options"]:
-    disk_type = build_settings["systems"][platform]["options"]["deploy_disk_type"]
-    if disk_type == "grub-iso":
-        if shutil.which("grub-file") is None:
-            cprint("grub-file is not installed!", "red")
-            exit(1)
+    # Get the deploy type of the system
+    deploy_type = build_settings["systems"][platform]["deploy_type"]
 
-        if build_settings["systems"][platform]["options"]["arch"] == "x86":
-            check_type = "--is-x86-multiboot"
+    # Make sure the deploy type is valid
+    if deploy_type in deploy_configs:
+
+        # Make sure the required tools are actually there
+        for x in deploy_configs[deploy_type]["required_tools"]:
+
+            # Warn the user is this is not true
+            if shutil.which(x) is None:
+                cprint(x + " is not installed, which is needed for deploying", "red")
+                exit(1)
+
+        # Make sure the script exists
+        if os.path.exists("../buildscripts/" + deploy_configs[deploy_type]["script"]):
+
+            # Run that script
+            if os.system("python3 ../buildscripts/" + deploy_configs[deploy_type]["script"] + " " + platform) != 0:
+                cprint("Buildscript failed!", "red")
+                exit(1)
         else:
-            cprint("Cannot deploy this kernel type yet to " + disk_type, "yellow")
-
-        # Verify that the nucleus produced is actually not malformed
-        if os.system("grub-file " + check_type + " nucleus") != 0:
-            cprint("The Nucleus is malformed (not multiboot complaint)", "red")
-            exit(1)
-
-        if shutil.which("grub-mkrescue") is None:
-            cprint("grub-mkrescue is not installed!", "red")
-            exit(1)
-
-    elif disk_type == "gba-rom":
-        if build_settings["systems"][platform]["options"]["arch"] != "arm":
-            cprint("The disk_type " + disk_type +
-                   " can only be used in arm", "red")
-            exit(1)
-
-        # Make sure its install
-        if shutil.which("llvm-objcopy-12") is None:
-            cprint("llvm-objcopy-12 is not installed!", "red")
-            exit(1)
-
-        # Convert to binary
-        if os.system("llvm-objcopy-12 -O binary nucleus") != 0:
-            cprint("objcopy failed on given nucleus image!", "red")
-            exit(1)
-
-        # Make sure gba fix is installed
-        if shutil.which("tsos-gbafix") is None:
-            cprint("tsos-gbafix is not installed!", "red")
-            exit(1)
-
-        # Fix the gba rom
-        if os.system("tsos-gbafix nucleus") != 0:
-            cprint("tsos-gbafix failed!", "red")
-            exit(1)
-
-    elif disk_type == "nspire_boot_partition":
-        if shutil.which("llvm-objcopy-12") is None:
-            cprint("llvm-objcopy-12 is not installed!", "red")
-            exit(1)
-
-        if os.system("llvm-objcopy-12 -O binary nucleus") != 0:
-            cprint("objcopy failed on given nucleus image!", "red")
+            cprint("Buildscript \"" +
+                   deploy_configs[deploy_type]["script"] + "\" could not be found", "red")
             exit(1)
 
     else:
-        cprint("Cannot yet deploy this system", "yellow")
+        cprint("Could not find the deploy type \"" +
+               deploy_type + "\" in the system config!", "red")
+        exit(1)
+
+
+else:
+    cprint("Cannot yet deploy this system", "yellow")
+
+
+# Building was succesful if it made it to here
+cprint("Compiling and deploying completed.", "green")
+cprint("Your copy of the TS/OS nucleus is at build/nucleus", "green")
 
 # Meaning that the user didn't want any other action
 if action == "":
     exit(0)
 
+# Make sure you are either debugging or configuring
 if action in ["debug", "test"]:
 
+    # Make sure the correct testing tools are there
+    for x in build_settings["systems"][platform]["required_test_tools"]:
+
+        # Warn the user if not
+        if shutil.which(x) is None:
+            cprint(x + " is not installed, which is needed for " + action, "red")
+            exit(1)
+
+    # Run said command
     if build_settings["systems"][platform][action+"_command"] != "":
-        os.system(build_settings["systems"][platform][action+"_command"])
+
+        # Make sure command doesn't fail
+        if os.system(build_settings["systems"][platform][action+"_command"]) != 0:
+            cprint("Action " + action + "failed", "red")
+            exit(1)
 
     else:
         cprint("Cannot " + action + " this system with this script yet.", "yellow")
         exit(0)
+
 else:
     cprint("Invalid action", "red")
+    exit(1)
