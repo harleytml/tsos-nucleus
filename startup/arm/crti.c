abbdff32c09073e37ab7597a789cbc55ecc3c1ab
tsuki-superior <tsukisuperior@tutanota.com>
2021-03-27 14:03:21 -0500

    formatting

--- startup/arm/crti.c
+++ startup/arm/crti.c
@@ -1,7 +1,9 @@
 typedef void (*func_ptr)(void);
 
-extern func_ptr _init_array_start[0], _init_array_end[0];
-extern func_ptr _fini_array_start[0], _fini_array_end[0];
+extern func_ptr _init_array_start[0];
+extern func_ptr _init_array_end[0];
+extern func_ptr _fini_array_start[0];
+extern func_ptr _fini_array_end[0];
 
 void _init(void)
 {
