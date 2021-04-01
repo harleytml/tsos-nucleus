728310d6712b4696b9930b03f5110ec741341d63
tsuki-superior <tsukisuperior@tutanota.com>
2021-04-01 16:55:30 -0500

    lol lets make this confirm to standards

--- buildscripts/deploy_gba.py
+++ buildscripts/deploy_gba.py
@@ -3,15 +3,16 @@
 
 import os
 import json
+import sys
 
 from termcolor import cprint
 
 # Convert to binary
 if os.system("llvm-objcopy-12 -O binary nucleus") != 0:
     cprint("objcopy failed on given nucleus image!", "red")
-    exit(1)
+    sys.exit(1)
 
 # Fix the gba rom
 if os.system("tsos-gbafix nucleus") != 0:
     cprint("tsos-gbafix failed!", "red")
-    exit(1)
+    sys.exit(1)
--- buildscripts/deploy_pc.py
+++ buildscripts/deploy_pc.py
@@ -3,14 +3,15 @@
 
 import os
 import json
+import sys
 
 from termcolor import cprint
 
 # Verify that the nucleus produced is actually not malformed
 if os.system("grub-file --is-x86-multiboot nucleus") != 0:
     cprint("The Nucleus is malformed (not multiboot complaint)", "red")
-    exit(1)
+    sys.exit(1)
 
 if os.system("grub-script-check ../misc/grub.cfg") != 0:
     cprint("The grub config file is not valid", "red")
-    exit(1)
+    sys.exit(1)
