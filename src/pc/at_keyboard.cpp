bbd001b14f169b2e3fa141bf1ef8c6e9c3bf0359
tsuki-superior <tsukisuperior@tutanota.com>
2021-04-02 14:25:38 +0000

    lol reformatting

--- src/pc/at_keyboard.cpp
+++ src/pc/at_keyboard.cpp
@@ -13,22 +13,16 @@ AT_KEYBOARD_quark::~AT_KEYBOARD_quark(void)
 bool AT_KEYBOARD_quark::detectsystem(void)
 {
 
-  /* Check the first keyboard flag bitmap
-    Bit 4 equaling 1 means a AT keyboard */
+  /* 
+   * Check the first keyboard flag bitmap
+   * Bit 4 equaling 1 means a AT keyboard 
+   */
   return (*((uint8_t *)0x496) & 0x10) == 0x10;
 }
 
 void AT_KEYBOARD_quark::reset(void)
 {
 }
-
-/*
-So detecting keys in PC is a little sketchy.
-The keyboard buffer is a cyclic queue
-hence you have to read characters before it is filled and starts to overwrite itself
-and this starts to happen, your BIOS will beep at you
-If you are testing TS/OS, please watch for this 
-*/
 
 key_identifier AT_KEYBOARD_quark::getkey(void)
 {
