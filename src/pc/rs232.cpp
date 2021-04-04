4f0f9b2a1cc56eb060552eff024313114aa69c1d
tsuki-superior <tsukisuperior@tutanota.com>
2021-04-04 14:52:36 -0500

    pc changes

--- src/pc/rs232.cpp
+++ src/pc/rs232.cpp
@@ -1,5 +1,7 @@
-//Quark by Tsuki Superior
+// Quark by Tsuki Superior
+// Reference for serial is https://github.com/Sayan505/MuffinOS/tree/master/kernel/sys/devices/serial
 #include "pc/rs232.hpp"
+#include "generic/nucleus_instance.hpp"
 
 RS232_quark::RS232_quark(void)
 {
@@ -13,11 +15,19 @@ bool RS232_quark::detectsystem(void)
 
 void RS232_quark::reset(void)
 {
+  tsos->io.out8(COM1 + 1, 0x00);
+  tsos->io.out8(COM1 + 3, 0x80);
+
+  set_baud(COM1, BAUD_115200); // set baud rate to 115200
+
+  tsos->io.out8(COM1 + 3, 0x03);
+  tsos->io.out8(COM1 + 2, 0xC7);
+  tsos->io.out8(COM1 + 4, 0x0B);
 }
 
 bool RS232_quark::isdevicethere(void)
 {
-  return false;
+  return true;
 }
 
 void RS232_quark::sendbyte(uint8_t byte)
@@ -27,4 +37,8 @@ void RS232_quark::sendbyte(uint8_t byte)
 uint8_t RS232_quark::getbyte(void)
 {
   return 0;
+}
+
+void RS232_quark::set_baud(uint16_t port, uint8_t baud_rate)
+{
 }
