59cf0130d6c3bd455abc238f875ff6c303a74d54
Tsuki Superior <77216999+tsuki-superior@users.noreply.github.com>
2021-04-05 20:18:20 -0500

    Create deploy_cubieboard1.py

---
+++ buildscripts/deploy_cubieboard1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# By Tsuki Superior
+
+import os
+import sys
+import shutil
+
+from termcolor import cprint
+
+shutil.copyfile("nucleus.elf", "nucleus")
+
+if os.system("llvm-objcopy -O binary nucleus") != 0:
+    cprint("objcopy failed on given nucleus image!", "red")
+    sys.exit(1)
