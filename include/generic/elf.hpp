b6aa91db615f99db0e71854b10e19679b5af92fd
tsuki-superior <tsukisuperior@tutanota.com>
2021-04-06 12:39:10 -0500

    formatting

--- include/generic/elf.hpp
+++ include/generic/elf.hpp
@@ -7,7 +7,6 @@
  * Reference: https://stackoverflow.com/questions/34960383/how-read-elf-header-in-c
  */
 
-
 #ifndef __TSOS_ELF_QUARK__
 #define __TSOS_ELF_QUARK__
 
@@ -27,51 +26,51 @@ public:
 private:
   class Elf64_header
   {
-    public:
-      uint8_t     e_ident[16];         /* Magic number and other info */
-      uint16_t    e_type;              /* Object file type */
-      uint16_t    e_machine;           /* Architecture */
-      uint32_t    e_version;           /* Object file version */
-      uint64_t    e_entry;             /* Entry point virtual address */
-      uint64_t    e_phoff;             /* Program header table file offset */
-      uint64_t    e_shoff;             /* Section header table file offset */
-      uint32_t    e_flags;             /* Processor-specific flags */
-      uint16_t    e_ehsize;            /* ELF header size in bytes */
-      uint16_t    e_phentsize;         /* Program header table entry size */
-      uint16_t    e_phnum;             /* Program header table entry count */
-      uint16_t    e_shentsize;         /* Section header table entry size */
-      uint16_t    e_shnum;             /* Section header table entry count */
-      uint16_t    e_shstrndx;          /* Section header string table index */
-  }__attribute__((packed));
+  public:
+    uint8_t e_ident[16];  /* Magic number and other info */
+    uint16_t e_type;      /* Object file type */
+    uint16_t e_machine;   /* Architecture */
+    uint32_t e_version;   /* Object file version */
+    uint64_t e_entry;     /* Entry point virtual address */
+    uint64_t e_phoff;     /* Program header table file offset */
+    uint64_t e_shoff;     /* Section header table file offset */
+    uint32_t e_flags;     /* Processor-specific flags */
+    uint16_t e_ehsize;    /* ELF header size in bytes */
+    uint16_t e_phentsize; /* Program header table entry size */
+    uint16_t e_phnum;     /* Program header table entry count */
+    uint16_t e_shentsize; /* Section header table entry size */
+    uint16_t e_shnum;     /* Section header table entry count */
+    uint16_t e_shstrndx;  /* Section header string table index */
+  } __attribute__((packed));
 
   class Elf32_header
   {
-    public:
-      uint8_t     e_ident[16];         /* Magic number and other info */
-      uint16_t    e_type;              /* Object file type */
-      uint16_t    e_machine;           /* Architecture */
-      uint32_t    e_version;           /* Object file version */
-      uint32_t    e_entry;             /* Entry point virtual address */
-      uint32_t    e_phoff;             /* Program header table file offset */
-      uint32_t    e_shoff;             /* Section header table file offset */
-      uint32_t    e_flags;             /* Processor-specific flags */
-      uint16_t    e_ehsize;            /* ELF header size in bytes */
-      uint16_t    e_phentsize;         /* Program header table entry size */
-      uint16_t    e_phnum;             /* Program header table entry count */
-      uint16_t    e_shentsize;         /* Section header table entry size */
-      uint16_t    e_shnum;             /* Section header table entry count */
-      uint16_t    e_shstrndx;          /* Section header string table index */
-  }__attribute__((packed));
+  public:
+    uint8_t e_ident[16];  /* Magic number and other info */
+    uint16_t e_type;      /* Object file type */
+    uint16_t e_machine;   /* Architecture */
+    uint32_t e_version;   /* Object file version */
+    uint32_t e_entry;     /* Entry point virtual address */
+    uint32_t e_phoff;     /* Program header table file offset */
+    uint32_t e_shoff;     /* Section header table file offset */
+    uint32_t e_flags;     /* Processor-specific flags */
+    uint16_t e_ehsize;    /* ELF header size in bytes */
+    uint16_t e_phentsize; /* Program header table entry size */
+    uint16_t e_phnum;     /* Program header table entry count */
+    uint16_t e_shentsize; /* Section header table entry size */
+    uint16_t e_shnum;     /* Section header table entry count */
+    uint16_t e_shstrndx;  /* Section header string table index */
+  } __attribute__((packed));
 
   enum e_type
   {
-    ET_NONE=0,
-    ET_REL=1,
-    ET_EXEC=2,
-    ET_DYN=3,
-    ET_CORE=4,
-    ET_LOPROC=0xff00,
-    ET_HIPROC0xffff
+    ET_NONE = 0,
+    ET_REL = 1,
+    ET_EXEC = 2,
+    ET_DYN = 3,
+    ET_CORE = 4,
+    ET_LOPROC = 0xff00,
+    ET_HIPROC = 0xffff
   };
 };
 
