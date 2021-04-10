061c15499cc7c8b62d375cc5abf479b51a4bc95a
tsuki-superior <tsukisuperior@tutanota.com>
2021-04-10 17:02:45 -0500

    can make isos now

--- buildscripts/deploy_pc.py
+++ buildscripts/deploy_pc.py
@@ -8,6 +8,29 @@ import json
 
 from termcolor import cprint
 
+# Open that config file and read from it
+tmp_file = open("../config/directory_structure.json")
+fileystem_dict = json.loads(tmp_file.read())
+tmp_file.close()
+
+root_dir = os.getcwd()
+if not(os.path.exists("filesystem")):
+    os.mkdir("filesystem")
+
+
+def recurse(dic):
+    def one_directory(dic, path):
+        for name, info in dic.items():
+            next_path = path + "/" + name
+            if isinstance(info, dict):
+                if not(os.path.exists(next_path)):
+                    os.mkdir(next_path)
+                one_directory(info, next_path)
+    one_directory(dic, "filesystem")
+
+
+recurse(fileystem_dict)
+
 # No conversion has to be done
 shutil.copyfile("nucleus.elf", "nucleus")
 
@@ -19,4 +42,10 @@ if os.system("grub-file --is-x86-multiboot nucleus") != 0:
 # Make sure that grub script is valid
 if os.system("grub-script-check ../misc/grub.cfg") != 0:
     cprint("The grub config file is not valid", "red")
+    sys.exit(1)
+
+shutil.copy("../misc/grub.cfg", "./filesystem/boot/")
+
+if os.system("grub-mkrescue -o nucleus.iso filesystem") != 0:
+    cprint("Grub Rescue Failed", "red")
     sys.exit(1)
