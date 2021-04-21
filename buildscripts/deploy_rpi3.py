a84868304c9a4c04aba043c9af85bfb260461c63
Tsuki Superior <tsukisuperior@tutanota.com>
2021-04-21 16:16:29 -0500

    fixed rpi3

--- buildscripts/deploy_rpi3.py
+++ buildscripts/deploy_rpi3.py
@@ -21,10 +21,9 @@ shutil.copy("../contrib/rpi-firmware/boot/LICENCE.broadcom", "./filesystem/")
 shutil.copy("../misc/config.txt", "./filesystem/")
 
 with open("nucleus.img", "wb") as f:
-    f.seek(1024 * 1024 * 50) # 50 MB
-    f.write(bytes('\x00'))
+    f.write(bytes(1024 * 1024 * 50))
     f.close()
-    
+
 # Make the image into a vfat drive
 if os.system("mkfs.fat -s1 -F32 -nTSOS \"nucleus.img\"") != 0:
     cprint("mkfs failed!", "red")
