98e0e51a0577023b412a204946ecfd706d88ad03
Tsuki Superior <tsukisuperior@tutanota.com>
2021-04-21 17:18:49 -0500

    uart almost done

--- src/rpi3/rpi3_uart.cpp
+++ src/rpi3/rpi3_uart.cpp
@@ -20,18 +20,18 @@ void RPI3_UART_quark::reset(void)
 	// Setup the GPIO pin 14 && 15.
 
 	// Disable pull up/down for all GPIO pins & delay for 150 cycles.
-	mmio_write((uint32_t)registers::GPPUD, 0x00000000);
+	tsos->io.out32(static_cast<uint32_t>(registers::GPPUD), 0x00000000);
 	delay(150);
 
 	// Disable pull up/down for pin 14,15 & delay for 150 cycles.
-	mmio_write((uint32_t)registers::GPPUDCLK0, (1 << 14) | (1 << 15));
+	tsos->io.out32(static_cast<uint32_t>(registers::GPPUDCLK0), (1 << 14) | (1 << 15));
 	delay(150);
 
 	// Write 0 to GPPUDCLK0 to make it take effect.
-	mmio_write((uint32_t)registers::GPPUDCLK0, 0x00000000);
+	tsos->io.out32(static_cast<uint32_t>(registers::GPPUDCLK0), 0x00000000);
 
 	// Clear pending interrupts.
-	mmio_write((uint32_t)registers::UART0_ICR, 0x7FF);
+	tsos->io.out32(static_cast<uint32_t>(registers::UART0_ICR), 0x7FF);
 
 	// Set integer & fractional part of baud rate.
 	// Divider = UART_CLOCK/(16 * Baud)
@@ -42,13 +42,14 @@ void RPI3_UART_quark::reset(void)
 	// Set it to 3Mhz so that we can consistently set the baud rate
 	// UART_CLOCK = 30000000;
 	uint32_t r = ((*(uint32_t *)(&mbox) & ~0xF) | 8);
+
 	// wait until we can talk to the VC
-	while (mmio_read((uint32_t)registers::MBOX_STATUS) & 0x80000000)
+	while (tsos->io.in32(static_cast<uint32_t>(registers::MBOX_STATUS)) & 0x80000000)
 	{
 	}
 	// send our message to property channel and wait for the response
 	mmio_write((uint32_t)registers::MBOX_WRITE, r);
-	while ((mmio_read((uint32_t)registers::MBOX_STATUS) & 0x40000000) || mmio_read((uint32_t)registers::MBOX_READ) != r)
+	while ((tsos->io.in32(static_cast<uint32_t>(registers::MBOX_STATUS)) & 0x40000000) || mmio_read((uint32_t)registers::MBOX_READ) != r)
 	{
 	}
 
@@ -91,6 +92,7 @@ uint8_t RPI3_UART_quark::inbyte(void)
 void RPI3_UART_quark::mmio_write(uint32_t reg, uint32_t data)
 {
 }
+
 uint32_t RPI3_UART_quark::mmio_read(uint32_t reg)
 {
 	return 0;
