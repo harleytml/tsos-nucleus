7e9d950dbdb91669bd8b6e5256313c55e99c69c1
Tsuki Superior <tsukisuperior@tutanota.com>
2021-04-22 20:31:58 -0500

    reformatting some stuff

--- lib/arm/__aeabi_idivmod.s
+++ lib/arm/__aeabi_idivmod.s
@@ -1,10 +1,10 @@
-	.global	__aeabi_idivmod
-	.align
+  .global	__aeabi_idivmod
+  .align
 
 __aeabi_idivmod:
-	stmfd	sp!, {r0, r1, ip, lr}
-	bl	__aeabi_idiv
-	ldmfd	sp!, {r1, r2, ip, lr}
-	mul	r3, r0, r2
-	sub	r1, r1, r3
-	mov	pc, lr
+  stmfd	sp!, {r0, r1, ip, lr}
+  bl	__aeabi_idiv
+  ldmfd	sp!, {r1, r2, ip, lr}
+  mul	r3, r0, r2
+  sub	r1, r1, r3
+  mov	pc, lr
