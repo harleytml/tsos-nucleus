20bbbb1d8144a20edd4b221712c4471083ff2bdd
Tsuki Superior <tsukisuperior@tutanota.com>
2021-04-22 20:19:08 -0500

    starting to add interrupts

---
+++ include/generic/interrupt_nucleon.hpp
@@ -0,0 +1,28 @@
+/* By Tsuki Superior
+ * Generic Interrupt Nucleon
+ *
+ * This nucleon, like the other nucleons, acts as a wrapper to call the quarks
+ * It has one more function, known as the fission, which is like the BSOD of
+ * Windows, or the Kernel Panic of the *nix operating systems
+ */
+
+#pragma once
+
+#include <generic/quark.hpp>
+#include <generic/nucleon.hpp>
+#include <generic/types.hpp>
+#include <generic/interrupt_quark.hpp>
+#include <generic/current_config.hpp>
+
+// The main class controlling the system runtime
+class Interrupt : public Nucleon<Interrupt_quark>
+{
+public:
+  // Constructor
+  Interrupt(void);
+
+  // Constructor
+  ~Interrupt();
+
+  void registerinterrupt();
+};
---
+++ include/generic/interrupt_quark.hpp
@@ -0,0 +1,21 @@
+/* By Tsuki Superior
+ * Abstract Interrupt Quark
+ * 
+ * To make a new Interrupt quark, simply have it inherit from this class
+ * 
+ */
+
+#pragma once
+
+#include <generic/quark.hpp>
+#include <generic/types.hpp>
+
+class Interrupt_quark : public Quark
+{
+public:
+  //Constructor
+  Interrupt_quark(void);
+
+  //Destructor
+  ~Interrupt_quark();
+};
